fefdee20109e50434d04c0ed147087bf766f09ba
Lars Grammel <lars.grammel@gmail.com>
2024-01-01 16:54:54 +0100

    Remove comment.

--- src/ExampleImageGenerationModel.ts
+++ src/ExampleImageGenerationModel.ts
@@ -21,28 +21,6 @@ import {
   mapBasicPromptToExampleFormat,
 } from "./ExampleImageGenerationPrompt.js";
 
-/**
- * Create an image generation model that calls the Stability AI image generation API.
- *
- * @see https://api.stability.ai/docs#tag/v1generation/operation/textToImage
- *
- * @example
- * const image = await generateImage(
- *   new StabilityImageGenerationModel({
- *     model: "stable-diffusion-512-v2-1",
- *     cfgScale: 7,
- *     clipGuidancePreset: "FAST_BLUE",
- *     height: 512,
- *     width: 512,
- *     samples: 1,
- *     steps: 30,
- *   })
- *   [
- *     { text: "the wicked witch of the west" },
- *     { text: "style of early 19th century painting", weight: 0.5 },
- *   ]
- * );
- */
 export class ExampleImageGenerationModel
   extends AbstractModel<ExampleImageGenerationSettings>
   implements
